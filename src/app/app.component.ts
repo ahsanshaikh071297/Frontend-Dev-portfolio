fe07bd0770bfd9ecb78d969d7a417df113feb5e9
ahsan shaikh <ahsanshaikh071297@outlook.com>
2024-04-18 14:40:20 +0530

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portfolio';
+  navVisible = false;
+
+  constructor (private elementRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    jQuery(this.elementRef.nativeElement).find('.item-wrap a').magnificPopup({
+      type: 'inline',
+      fixedContentPos: false,
+      removalDelay: 300,
+      showCloseBtn: false,
+      mainClass: 'mfp-fade'
+    });
+
+    jQuery(document).on('click', '.popup-modal-dismiss', function (e:any) {
+      e.preventDefault();
+      jQuery.magnificPopup.close();
+    });
+  }
+
+  openPopup() {
+    // Open the popup manually if needed
+    jQuery.magnificPopup.open({
+      items: {
+        src: '#popup-content'
+      },
+      type: 'inline'
+    });
+  }
+
+  closePopup() {
+    // Close the popup manually if needed
+    jQuery.magnificPopup.close();
+  }
+
+  toggleMenu() {
+    this.navVisible = !this.navVisible;
+  }
+
+  closeMenu() {
+    this.navVisible = false;
+  }
+
+  downloadResume() {
+    const resumeUrl = '/assets/Ahsan_Shaikh-1-2-2.pdf'; // Replace with the actual path to your resume file
+    const anchor = document.createElement('a');
+    anchor.href = resumeUrl;
+    anchor.download = 'ahsan_resume.pdf'; // Specify the desired filename for download
+    anchor.click();
+  }
 }
